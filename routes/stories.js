57266cb006a745bf4496bfe980bbf3177dead45c
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-04-09 17:27:39 -0400

    holy moses

--- routes/stories.js
+++ routes/stories.js
@@ -50,6 +50,26 @@ router.get('/', ensureAuth, async (req, res) => {
     }
 })
 
+// @desc Show single story
+// @ route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try {
+        let story = await Story.findById(req.params.id)
+            .populate('user')
+            .lean()
+        if (!story) {
+            return res.render('error/404')
+        }
+
+        res.render('stories/show', {
+            story
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/404')
+    }
+})
+
 // @desc Show edit
 // @ route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -118,5 +138,26 @@ router.delete('/:id', ensureAuth, async (req, res) => {
         return res.render('error/500')
     }
 })
+
+// @desc    User stories
+// @route   GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({
+                user: req.params.userId,
+                status: 'public',
+            })
+            .populate('user')
+            .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.error(err)
+        res.render('error/500')
+    }
+})
+
 
 module.exports = router
