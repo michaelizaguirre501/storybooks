190a78e501a127ae88eb9a3d58ec45bd6461c115
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-04-28 17:36:47 -0400

    heroku

--- routes/stories.js
+++ routes/stories.js
@@ -38,7 +38,7 @@ router.get('/', ensureAuth, async (req, res) => {
             })
             .lean()
 
-        res.render('/stories/index', {
+        res.render('stories/index', {
             stories,
         })
     } catch (err) {
