3ef57c58096ecad8c8955ac2be0064a34d3fb3ba
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-04-28 21:22:20 -0400

    heroku

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
-app.use('/stories', require('./routes/stories'))
+app.use('stories', require('./routes/stories'))
 
 
 
